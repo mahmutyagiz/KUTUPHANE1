db26609c02a5954a42472a5dca3c2ee4ed7fbd57
mahmut yağız ekşi <164015661+mahmutyagiz@users.noreply.github.com>
2024-10-15 11:15:16 +0300

    Add files via upload

---
+++ araba.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+public class Kitap
+{
+    public string KitapAdi { get; set; }
+    public string YazarAdi { get; set; }
+    public string ISBN { get; set; }
+    public int YayinYili { get; set; }
+
+    public Kitap(string kitapAdi, string yazarAdi, string isbn, int yayinYili)
+    {
+        KitapAdi = kitapAdi;
+        YazarAdi = yazarAdi;
+        ISBN = isbn;
+        YayinYili = yayinYili;
+    }
+
+    public void KitapBilgisiGoster()
+    {
+        Console.WriteLine($"Kitap Adı: {KitapAdi}, Yazar: {YazarAdi}, ISBN: {ISBN}, Yayın Yılı: {YayinYili}");
+    }
+}
